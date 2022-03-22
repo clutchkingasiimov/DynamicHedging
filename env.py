9042a90bf7d65bee249bafac47d56fe92499ed52
clutchkingasiimov <saureyj43@gmail.com>
2022-03-22 02:41:41 +0100

    Commited changes to the reset and step function

--- env.py
+++ env.py
@@ -25,7 +25,7 @@ class TradingEnv(gym.Env):
     def __init__(self,num_simulations=int,num_contracts=int,
     multiplier=float,tick_size=float,kappa=float):
 
-        self.num_simulations = 100
+        self.num_simulations = num_simulations
         self.num_contracts = num_contracts 
         self.multiplier = multiplier 
         self.tick_size = tick_size
@@ -34,9 +34,10 @@ class TradingEnv(gym.Env):
         os = OptionSimulation(100,self.num_simulations) 
         
         #can add a maturity term
-        self.sim_prices = os.GBM(50,0.05,time_increment=1)
+        self.sim_prices = os.GBM(50,0.1,time_increment=1)
+        self.days_to_expiry_normalized = os.ttm/self.trading_days #Only to be used for the calculation of BS call price
         self.days_to_expiry = os.ttm #Creates an array of days left to expiry 
-        self.option_price_path, self.option_delta_path = os.BS_call(self.days_to_expiry,self.sim_prices,100,0.01,0,0)
+        self.option_price_path, self.option_delta_path = os.BS_call(self.days_to_expiry_normalized,self.sim_prices,100,0.01,0,0)
         
         #Action space (Discrete)
         self.num_actions = self.num_contracts*self.num_of_shares #Number of actions 
@@ -44,7 +45,7 @@ class TradingEnv(gym.Env):
 
         if self.num_contracts > 10:
             raise ValueError("The maximum number of contracts in the simulation cannot be more than 10.")
-
+            
     def _cost_of_trade(self,n):
         #n: Number of shares 
         cost = self.multiplier * self.tick_size * (np.abs(n) * 0.01*n*n)
@@ -63,46 +64,65 @@ class TradingEnv(gym.Env):
         Parameters:
             pt: Price at time 't'
             n: Number of shares at time 't'
+
+        Returns: 
+            rwd: The reward value from the trade
         '''
         wt = self._wealth_of_trade(pt, n)
         rwd = wt - (self.kappa*0.5)*(wt**2) 
         return rwd 
     
     def reset(self, path):
+        '''
+        Resets the environment in order to start a new episode for the simulation
+
+        Parameters:
+            path: The path the agent is following 
+
+        Returns: 
+            self.state: The state vector of the agent
+        '''
         # repeatedly go through available simulated paths (if needed)
         self.t = 0
         self.path = path
         ttm = self.days_to_expiry[0]-1
 
-        n = 500 #no of shares
-        #print(self.sim_prices[self.path])
-        #print(self.sim_prices[self.path,0])
-        #print(self.option_delta_path[self.path,self.t])
+        n = self.num_of_shares #no of shares
         
         #K : strike price (to be given)
 
         price =  round(self.sim_prices[self.path,self.t])
-        self.nt = n
+        self.nt = n #Number of shares at time 't'
         price_ttm = round(self.sim_prices[self.path,ttm])
         
-        self.state = [price , ttm, self.nt, price_ttm]
+        self.state = [price, ttm, self.nt, price_ttm]
 
         return self.state
     
     def delta(self, ttm):
+        #Returns the option delta 
         delta = self.option_delta_path[self.path, ttm]
         return delta
 
     def step(self,action):
-        
+        '''
+        Step function to allow the agent to transition into the next state of the episode 
+
+        Parameters: 
+            action: The action the agent takes
+
+        Returns: 
+            self.state: The state vector of the agent 
+            R: The reward value 
+            done: Boolean value of whether the episode is over or not
+        '''
         self.t = self.t + 1 
         price =  round(self.sim_prices[self.path,self.t])
         self.nt = self.nt + action
         ttm = self.days_to_expiry[self.t] 
         price_ttm = round(self.sim_prices[self.path,ttm])
         
-        R = round(self.reward(price, self.nt)) 
-        
+        reward = round(self.reward(price, self.nt)) 
         self.state = [price , ttm, self.nt, price_ttm]
         
         if ttm == 0:
@@ -110,10 +130,9 @@ class TradingEnv(gym.Env):
         else:
             done = False
         
-        return self.state, R, done
+        return self.state, reward, done
 
 
-    
 if __name__ == "__main__":
     import matplotlib.pyplot as plt 
     env = TradingEnv(num_simulations=100,num_contracts=5,multiplier=1.0,
